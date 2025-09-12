03bcea9624cc78cfe890d336f1ec2a67141d23a3
tianyi <3471215064@qq.com>
2025-09-12 16:53:54 +0800

    新增钩子

--- data.js
+++ data.js
@@ -1,24 +1,32 @@
 const dataList = [
   {
     id: 1,
-    title: '电视剧01',
-    btnTitle: '电视剧01抢先看',
-    searchTitle: '电视剧01完整版',
-    images: ['img/image-1-1.jpg', 'img/image-1-2.jpg']
-  },
-  {
-    id: 2,
-    title: '电视剧02',
-    btnTitle: '电视剧02抢先看',
-    searchTitle: '电视剧02完整版',
-    images: ['img/image-2-1.jpg', 'img/image-2-2.jpg']
+    title: '赴山海',
+    btnTitle: '赴山海  抢先看',
+    searchTitle: '赴山海完整版',
+    images: ['img/image-1-1.jpg', 'img/image-1-2.jpg', 'img/shareImage1.jpg']
   },
   {
     id: 3,
-    title: '电视剧03',
-    btnTitle: '电视剧03抢先看',
-    searchTitle: '电视剧03完整版',
-    images: ['img/image-2-1.jpg', 'img/image-2-2.jpg']
+    title: '仙逆',
+    btnTitle: '仙逆  抢先看',
+    searchTitle: '仙逆完整版',
+    images: ['img/image-3-1.jpg', 'img/image-3-2.jpg', 'img/shareImage3.jpg']
+  },
+  {
+    id: 3,
+    title: '完美世界',
+    btnTitle: '完美世界  抢先看',
+    searchTitle: '完美世界完整版',
+    images: ['img/image-4-1.jpg', 'img/image-4-2.jpg', 'img/shareImage4.jpg']
+  },
+  {
+    id: 2,
+    title: '归队',
+    btnTitle: '归队  抢先看',
+    searchTitle: '归队完整版',
+    images: ['img/image-2-1.jpg', 'img/image-2-2.jpg', 'img/shareImage2.jpg']
   }
+  
 ]
 
